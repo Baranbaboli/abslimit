aa6debf5a5ada22f450e4dbe72e4d8995381478a
Baran ATLIHAN <43598215+Baranbaboli@users.noreply.github.com>
2018-10-17 20:17:00 +0300

    Add files via upload

---
+++ Baboli_Abs_limit_C/Baboli_Abs_limit_C/Source.c
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main()
+{  //main
+
+
+	int week, hour, myhour;
+	float abslimit;
+
+	printf("Enter number of weeks: ");
+	scanf("%d", &week);
+	printf("Enter number of hours:");
+	scanf("%d", &hour);
+
+	abslimit = week * hour*0.3;
+	printf("Absence Limit:");
+	printf("%.2f", abslimit);
+
+	printf("\nEnter your absences:");
+	scanf("%d", &myhour);
+
+	if (myhour > abslimit)
+		printf("YOU HAVE FAILED HONEY! Cry more \n");
+	else
+		printf("Nice, keep coming \n");
+
+	system("pause");
+
+
+	return 0;
+
+}
